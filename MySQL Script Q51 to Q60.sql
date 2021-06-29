5abd9aae70713e8c2083aa6457df2ce8491e7c3c
Devendra Mukati <devendra.mukati11@gmail.com>
2021-06-30 01:23:21 +0530

    Update MySQL Script Q51 to Q60.sql

--- MySQL Script Q51 to Q60.sql
+++ MySQL Script Q51 to Q60.sql
@@ -6,12 +6,12 @@
 select * from Customers where (rating<=100 and city='Rome') or rating>100;
 
 
-#52) Find all rows from the customer’s table for which the salesperson number is 1001.
+#52) Find all rows from the customerâ€™s table for which the salesperson number is 1001.
 
-select * from customers where snum=1001;
+select * from customers where snum=1001;
 
 
-#53) Find the total amount in orders for each salesperson where their total of amounts are greater than the amount of the largest order in the table.
+#53) Find the total amount in orders for each salesperson where their total of amounts are greater than the amount of the largest order in the table.
 
 select * from (select SUM(AMT) AS Total_Amount from orders GROUP BY SNUM) a where a.Total_Amount>(select MAX(AMT) from orders);
 
@@ -23,7 +23,9 @@ select onum from orders where amt is NULL or amt=0;
 
 #55) Produce all combinations of salespeople and customer names such that the former precedes the latter alphabetically, and the latter has a rating of less than 200.
 
-select s.sname, c.cname, c.rating from Customers c,salespeople s where c.rating<200 group by s.sname,c.cname,c.rating having c.cname>=s.sname;
+select s.sname, c.cname, c.rating from Customers c,salespeople s where c.rating<200 group by s.sname,c.cname,c.rating having c.cname>=s.sname;
 
 
-
+
+
+
